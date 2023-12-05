013c4456db272615046e884e4467f008da989cd6
Walls <blumvaido@gmail.com>
2023-12-05 05:38:38 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'PDL Help'
+author 'wallsxd'
+description 'Displays commands and keybind information'
+version '1.0.0'
+
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'server.lua',
+    'client.js'
+}
+
+dependencies {
+    'mysql-async'
+}
